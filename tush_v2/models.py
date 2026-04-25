23e44a3fbdc68519f77a91aa9ded3a3ea6e4cc08
Aryan <aundhad504@rku.ac.in>
2026-04-25 17:11:19 +0530

    Update models.py

--- tush_v2/models.py
+++ tush_v2/models.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
+    password = db.Column(db.String(256), nullable=False)
+    
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
