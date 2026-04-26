f2dda77682a9f59a8c4da4b9db6b851b9ac7a311
Aryan <aundhad504@rku.ac.in>
2026-04-26 11:23:02 +0530

    Update app.py

--- tush_v2/app.py
+++ tush_v2/app.py
@@ -34,6 +34,7 @@ with app.app_context():
                 os.makedirs('instance')
             except:
                 pass
+                 db.drop_all() 
     db.create_all()
     if not User.query.filter_by(username='admin').first():
         admin = User(username='admin', password=generate_password_hash('admin123'))
